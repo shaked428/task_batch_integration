af4efd206d4c865432f121528c30111b5ece2119
Robrecht Cannoodt <rcannood@gmail.com>
2024-09-17 23:24:42 +0200

    update process dataset

--- scripts/create_resources/test_resources.sh
+++ scripts/create_resources/test_resources.sh
@@ -6,11 +6,6 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 # ensure that the command below is run from the root of the repository
 cd "$REPO_ROOT"
 
-# remove this when you have implemented the script
-echo "TODO: replace the commands in this script with the sequence of components that you need to run to generate test_resources."
-echo "  Inside this script, you will need to place commands to generate example files for each of the 'src/api/file_*.yaml' files."
-exit 1
-
 set -e
 
 RAW_DATA=resources_test/common
@@ -19,17 +14,10 @@ DATASET_DIR=resources_test/task_batch_integration
 mkdir -p $DATASET_DIR
 
 # process dataset
-echo Running process_dataset
-nextflow run . \
-  -main-script target/nextflow/workflows/process_datasets/main.nf \
-  -profile docker \
-  --publish_dir "$DATASET_DIR" \
-  --id "pancreas" \
+viash run src/process_dataset/config.vsh.yaml -- \
   --input "$RAW_DATA/cxg_mouse_pancreas_atlas/dataset.h5ad" \
-  --output_train '$id/train.h5ad' \
-  --output_test '$id/test.h5ad' \
-  --output_solution '$id/solution.h5ad' \
-  --output_state '$id/state.yaml'
+  --output_dataset "$DATASET_DIR/cxg_mouse_pancreas_atlas/dataset.h5ad" \
+  --output_solution "$DATASET_DIR/cxg_mouse_pancreas_atlas/solution.h5ad"
 
 # run one method
 viash run src/methods/knn/config.vsh.yaml -- \
