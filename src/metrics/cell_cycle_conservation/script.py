f9f1417d48bff9f2f713d8a1176f55c63dbb3747
Robrecht Cannoodt <rcannood@gmail.com>
2025-01-17 16:34:22 +0100

    minor tweaks to the cell cycle component (#34)
    * minor tweaks to the cell cycle component
    
    * add flush

--- src/metrics/cell_cycle_conservation/script.py
+++ src/metrics/cell_cycle_conservation/script.py
@@ -1,21 +1,20 @@
 import sys
 import anndata as ad
 from scib.metrics import cell_cycle
-import numpy as np
 
 ## VIASH START
 par = {
     'input_integrated': 'resources_test/task_batch_integration/cxg_immune_cell_atlas/integrated_full.h5ad',
+    "input_solution": "resources_test/task_batch_integration/cxg_immune_cell_atlas/output_solution.h5ad",
     'output': 'output.h5ad'
 }
-
 meta = {
-    'name': 'foo'
+    "resources_dir": "target/executable/metrics/cell_cycle_conservation",
 }
 ## VIASH END
+
 sys.path.append(meta["resources_dir"])
 from read_anndata_partial import read_anndata
-
 
 print('Read input', flush=True)
 adata_solution = read_anndata(
@@ -25,12 +24,15 @@ adata_solution = read_anndata(
     var='var',
     uns='uns'
 )
+print(f"adata_solution: {adata_solution}", flush=True)
+
 adata_integrated = read_anndata(
     par['input_integrated'],
     obs='obs',
     obsm='obsm',
     uns='uns'
 )
+print(f"adata_integrated: {adata_integrated}", flush=True)
 
 print("Copy batch information", flush=True)
 adata_integrated.obs['batch'] = adata_solution.obs['batch']
@@ -38,21 +40,13 @@ adata_integrated.obs['batch'] = adata_solution.obs['batch']
 print('Use gene symbols for features', flush=True)
 adata_solution.var_names = adata_solution.var['feature_name']
 
-dataset_organism = adata_solution.uns['dataset_organism']
-translator = {
-    "homo_sapiens": "human",
-    "mus_musculus": "mouse",
-}
-assert dataset_organism in translator, \
-    f"dataset organism ({dataset_organism}) not in translator!"
-
 print('Compute score', flush=True)
 score = cell_cycle(
     adata_solution,
     adata_integrated,
     batch_key='batch',
     embed='X_emb',
-    organism=translator[dataset_organism],
+    organism=adata_solution.uns['dataset_organism'],
 )
 
 print('Create output AnnData object', flush=True)
@@ -65,7 +59,7 @@ output = ad.AnnData(
         'metric_values': [ score ]
     }
 )
-
+print(f"output: {output}", flush=True)
 
 print('Write data to file', flush=True)
 output.write_h5ad(par['output'], compression='gzip')
