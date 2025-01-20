bdeaaaf54fab9170063e135a34b98d8eb374d079
Luke Zappia <lazappi@users.noreply.github.com>
2025-01-20 15:30:26 +0100

    Replace "+" in batch names for liger (#37)

--- src/methods/liger/script.R
+++ src/methods/liger/script.R
@@ -14,6 +14,7 @@ meta <- list(
 
 cat("Read input\n")
 adata <- anndata::read_h5ad(par$input)
+adata$obs["batch"] <- sub("\\+", "plus", adata$obs[["batch"]]) # Replace "+"" characters in batch names
 
 anndataToLiger <- function(adata) {
   # fetch batch names
